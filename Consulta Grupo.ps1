38afb943b1a37d3d34dbab1d870569be43c6e5b8
leandro-a-pereira <leandro.pereira@casasbahia.com.br>
2025-05-02 12:53:17 -0300

    Update Consulta Grupo.ps1

--- Consulta Grupo.ps1
+++ Consulta Grupo.ps1
@@ -1,17 +1,7 @@
 $matriculas = get-content -Path C:\temp\1.txt
 foreach ($m in $matriculas)
 {
-$users = Get-ADPrincipalGroupMembership $m | where-object {$_.name -like "VPNRA_*"}
+$users = Get-ADPrincipalGroupMembership $m | where-object {$_.name -like "GRUPOTAL_*"}
 
 "Matricula $($m) Grupo $($users)" | Out-File c:\temp\11.txt -Append
 }
-
-
-
-
-#(Get-ADGroupMember VPN_TRANSICAO).count #relação D todos que estão no grupo transição
-
-#get-adgroupmember vpnra_cyber | fl name | Out-File c:\temp\teste.txt -Append
-
-
-#Get-ADGroupMember VPN_TRANSICAO | fl name | out-file c:\temp\vpn_transicao.txt
